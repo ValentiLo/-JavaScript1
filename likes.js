8921f8d6b067c218fa11dff2f9968ca16a0356ac
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2025-05-28 11:18:51 +0300

    Update likes.js

--- likes.js
+++ likes.js
@@ -1,5 +1,18 @@
+// let heart = document.querySelector('.heart');
+
+// heart.onclick = function () {
+//   heart.classList.toggle('added');
+// };
 let heart = document.querySelector('.heart');
+let likesNumber = document.querySelector('.likes-number');
+
 
 heart.onclick = function () {
+  if (heart.classList.contains('added')) {
+    likesNumber.textContent--;
+  } else {
+    likesNumber.textContent++;
+  }
+  
   heart.classList.toggle('added');
 };
