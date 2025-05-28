0254e0376773c51b8cef9b8646e2131d48490746
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2025-05-28 11:37:42 +0300

    Create comments.js

---
+++ comments.js
@@ -0,0 +1,8 @@
+let commentForm = document.querySelector('.comment-form');
+let commentList = document.querySelector('.comment-list');
+commentList.textContent = 'Новый комментарий';
+
+commentForm.onsubmit = function (evt) {
+  evt.preventDefault();
+
+};
