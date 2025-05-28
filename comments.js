5bc3dc10cff036916a30a4577da7321d0f53465e
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2025-05-28 11:59:32 +0300

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,8 +1,21 @@
+// let commentForm = document.querySelector('.comment-form');
+// let commentList = document.querySelector('.comment-list');
+// commentList.textContent = 'Новый комментарий';
+
+// commentForm.onsubmit = function (evt) {
+//   evt.preventDefault();
+
+// };
 let commentForm = document.querySelector('.comment-form');
 let commentList = document.querySelector('.comment-list');
-commentList.textContent = 'Новый комментарий';
+let commentField = document.querySelector('.comment-field');
 
 commentForm.onsubmit = function (evt) {
   evt.preventDefault();
 
+  let newComment = document.createElement('li');
+  newComment.classList.add('user-comment');
+  newComment.textContent = commentField.value;
+  commentField.value = '';
+  commentList.append(newComment);
 };
